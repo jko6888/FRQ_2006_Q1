05c92f562d8dc9bd46a568faebf86e98dbb3f4e7
james <jko6888@bths.edu>
2018-12-19 00:38:54 -0500

    Initial commit

---
+++ src/JamesKo/Appointment.java
@@ -0,0 +1,27 @@
+package JamesKo;
+
+public class Appointment {
+    public TimeInterval interval;
+
+    public Appointment(int startTime, int endTime)
+    {
+        this.interval = new TimeInterval(startTime, endTime);
+    }
+    public TimeInterval getTime()
+    {
+        return this.interval;
+    }
+
+    public boolean conflictsWith(Appointment Other)
+    {
+        if(Other.interval.overlapsWith(interval)) {
+            return true;
+        }
+        return false;
+    }
+
+    public String toString()
+    {
+        return "Start: " + this.interval.getStartTime() + ", End: " + this.interval.getEndTime() + "\n";
+    }
+}
---
+++ src/JamesKo/DailySchedule.java
@@ -0,0 +1,47 @@
+package JamesKo;
+
+import java.util.ArrayList;
+
+public class DailySchedule {
+    private ArrayList<Appointment> apptList;
+
+    public DailySchedule()
+    {
+        apptList = new ArrayList();
+    }
+
+    public void clearConflicts(Appointment appt)
+    {
+        for(int i = 0;i<apptList.size();i++)
+        {
+            if(appt.conflictsWith(apptList.get(i)));
+            {
+                apptList.remove(i);
+            }
+        }
+    }
+
+    public boolean addAppt(Appointment appt, boolean emergency)
+    {
+        if(emergency == true)
+        {
+            clearConflicts(appt);
+            apptList.add(appt);
+            return true;
+        }
+        else
+        {
+            boolean conflicts = false;
+            for (Appointment curAppt : apptList){
+                if(appt.conflictsWith(curAppt) && !conflicts) conflicts = true;
+            }
+            if (!conflicts) apptList.add(appt);
+            return conflicts;
+        }
+    }
+
+    public String toString()
+    {
+        return apptList.toString();
+    }
+}
